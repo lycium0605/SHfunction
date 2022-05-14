931781d09599c59dce87daec83fdfd6f2522e1f5
lycium0605 <51844050+lycium0605@users.noreply.github.com>
2022-05-14 19:31:40 -0400

    Update SR-related.R

--- R/SR-related.R
+++ R/SR-related.R
@@ -28,7 +28,7 @@ get_centrality <- function(my_focal, my_grp, my_subset) {
 
   # Calculate centrality metrics
   res <- f_graph %>%
-    tidygraph::morph(to_split, grp) %>%
+    tidygraph::morph(tidygraph::to_split, grp) %>%
     dplyr::mutate(eigen_wt = centrality_eigen(weights = res_i_adj)
            # eigen = centrality_eigen(),
            # betweenness = centrality_betweenness(normalized = TRUE),
